6f6718587b77202c229b55abacf8ff688dafb196
Alex Woods <alexwoods@Alexs-MacBook-Air.local>
2016-08-16 11:57:52 -0400

    merging everything

--- monte_carlo/game_views.py
+++ monte_carlo/game_views.py
@@ -1,5 +1,18 @@
 from monte_carlo import app
+from monte_carlo.components.models import Game
+
+games = []
+
 
 @app.route('/test', methods=['GET'])
 def test_method():
     return 'THIS IS A TEST'
+
+
+@app.route('/start-game', methods=['POST'])
+def create_game():
+    game = Game()
+    games.append(game)
+    return game
+
+
